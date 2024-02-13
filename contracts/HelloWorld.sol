2492dd4359109f9ca0ea9ab693d87d4221c7d1b4
Afraz Alam <afrazalam140@gmail.com>
2024-02-13 18:23:53 +0500

    Update HelloWorld.sol

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,1 +1,19 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract HelloWorld {
+    event UpdatedMessages(string oldStat, string newStat);
+
+    string public message;
+
+    constructor(string memory initMessage) {
+        message = initMessage;
+    }
+
+    function update(string memory newMessage) public {
+        string memory oldMsg = message;
+        message = newMessage;
+        emit UpdatedMessages(oldMsg, newMessage);
+    }
+}
